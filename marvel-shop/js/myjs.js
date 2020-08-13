5e1d40ed5e935fac2940700acd306d739ad5e481
= <=>
2020-08-13 18:55:39 +0700

    update js

--- marvel-shop/js/myjs.js
+++ marvel-shop/js/myjs.js
@@ -14,21 +14,23 @@ $(document).ready(function(){
 })
 
 
-// $(document).ready(function(){
-// 	$(document).on('click', '.list_img_detail', function(e) {
-// 		// e.preventDefault();
-// 		// $(this).css('border', 'solid 3px red');
-// 		// var src_this=$(this).attr('src');
-// 		// $('#at_img').attr('src',src_this);
+$(document).ready(function(){
+	$(document).on('click', '.list_img_detail', function(e) {
+		// e.preventDefault();
+		// $(this).css('border', 'solid 3px red');
+		// var src_this=$(this).attr('src');
+		// $('#at_img').attr('src',src_this);
 
 
-// 		// 	e.preventDefault();
-// 		// $(this).parent().addClass('addshadow');
-// 		// var src_this=$(this).attr('src');
-// 		// $('#at_img').attr('src',src_this);
-// 		// $(this).parent().prevAll().removeClass('addshadow');
-// 		// $(this).parent().nextAll().removeClass('addshadow');
+		e.preventDefault();
+		$(this).parent().addClass('addshadow');
+		var src_this=$(this).attr('src');
+		$('#at_img').attr('src',src_this);
+		$(this).parent().prevAll().removeClass('addshadow');
+		$(this).parent().nextAll().removeClass('addshadow');
 		
+		});
+		});
 		
 
 	
