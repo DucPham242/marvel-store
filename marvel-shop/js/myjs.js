bde04850e8db3d1e6413379873bae71336b14f67
= <=>
2020-08-13 16:34:51 +0700

    add new file user and detail user

--- marvel-shop/js/myjs.js
+++ marvel-shop/js/myjs.js
@@ -1,41 +1,34 @@
-<<<<<<< HEAD
 $(document).ready(function(){
+
+	// var nav = jQuery("nav").offset().top; // đo khoảng cách từ top đến nav
 	
-=======
-// $(document).ready(function(){
-// 	$('.dropdown').hover(function()
-// 	{
-// 		$('.dropdown-menu',this).stop(true,true).fadeIn("fast");
-// 			$(this).toggleClass('open');
-// 			$(b,this).toggleClass(caret caret-up);
-// 	}
-// 	function()
-// 	{
-
-// 	});
-// })
-
-$(document).ready(function(){
-	$(document).on('click', '.list_img_detail', function(e) {
-		// e.preventDefault();
-		// $(this).css('border', 'solid 3px red');
-		// var src_this=$(this).attr('src');
-		// $('#at_img').attr('src',src_this);
-
-
-			e.preventDefault();
-		$(this).parent().addClass('addshadow');
-		var src_this=$(this).attr('src');
-		$('#at_img').attr('src',src_this);
-		$(this).parent().prevAll().removeClass('addshadow');
-		$(this).parent().nextAll().removeClass('addshadow');
-		
-		
-
-		
-	
-		
-
-	});
->>>>>>> c4f78230ae314b6e5dfcadc13d39a15aa7f5dad8
+	// jQuery(window).scroll(function(){ // để mỗi khi cuộn dòng trong function sẽ được kích hoạt
+	// 	var scrollPos = jQuery(window).scrollTop(); //Hiện giá trị khi cuộn
+	// 	// jQuery(".status").html(scrollPos);
+	// 	if ( scrollPos >= nav) {
+	// 		jQuery("nav").addClass("fixed");
+	// 	}else {
+	// 		jQuery("nav").removeClass("fixed");
+	// 	}
+	// })
 })
+
+
+// $(document).ready(function(){
+// 	$(document).on('click', '.list_img_detail', function(e) {
+// 		// e.preventDefault();
+// 		// $(this).css('border', 'solid 3px red');
+// 		// var src_this=$(this).attr('src');
+// 		// $('#at_img').attr('src',src_this);
+
+
+// 		// 	e.preventDefault();
+// 		// $(this).parent().addClass('addshadow');
+// 		// var src_this=$(this).attr('src');
+// 		// $('#at_img').attr('src',src_this);
+// 		// $(this).parent().prevAll().removeClass('addshadow');
+// 		// $(this).parent().nextAll().removeClass('addshadow');
+		
+		
+
+	
